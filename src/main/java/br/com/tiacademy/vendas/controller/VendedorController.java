e805cb3ca576888d3e3f0592d2e2c283fe15dce7
TiagoTNX <tiagotnx@gmail.com>
2022-09-06 02:42:26 -0300

    :sparkles: Configuração do método GET - Service/Controller.

--- src/main/java/br/com/tiacademy/vendas/controller/VendedorController.java
+++ src/main/java/br/com/tiacademy/vendas/controller/VendedorController.java
@@ -1,13 +1,14 @@
 package br.com.tiacademy.vendas.controller;
 
 import br.com.tiacademy.vendas.domain.Vendedor;
+import br.com.tiacademy.vendas.service.VendedorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -15,35 +16,25 @@ import java.util.Objects;
 @RequestMapping("/vendedor")
 public class VendedorController {
 
+    @Autowired
+    private VendedorService vendedorService;
+
     @GetMapping
     public ResponseEntity<List<Vendedor>> listar() {
-        return ResponseEntity.ok(vendedores());
+        var vendedores = vendedorService.listar();
+        return ResponseEntity.ok(vendedores);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Vendedor> especifico(@PathVariable("id") Long id) {
-        var resultado = vendedores().stream().filter(vendedor -> vendedor.getId().equals(id))
-                .findFirst()
-                .orElse(null);
+
+        var resultado = vendedorService.porID(id);
 
         if (Objects.isNull(resultado)) {
             return ResponseEntity.notFound().build();
         }
 
         return ResponseEntity.ok(resultado);
-    }
-
-    private List<Vendedor> vendedores() {
-
-        Vendedor dois = new Vendedor();
-        dois.setId(2L);
-        dois.setNome("Manuel");
-
-        Vendedor um = new Vendedor();
-        um.setId(1L);
-        um.setNome("João");
-
-        return Arrays.asList(um, dois);
     }
 
 }
--- src/main/java/br/com/tiacademy/vendas/service/VendedorService.java
+++ src/main/java/br/com/tiacademy/vendas/service/VendedorService.java
@@ -1,8 +1,23 @@
 package br.com.tiacademy.vendas.service;
 
+import br.com.tiacademy.vendas.domain.Vendedor;
+import br.com.tiacademy.vendas.repository.VendedorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VendedorService {
-    
+
+    @Autowired
+    private VendedorRepository vendedorRepository;
+
+    public List<Vendedor> listar() {
+        return vendedorRepository.findAll();
+    }
+
+    public Vendedor porID(Long id) {
+        return vendedorRepository.findById(id).orElse(null);
+    }
 }
