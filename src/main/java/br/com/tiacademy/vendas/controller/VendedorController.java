6f0e3a8456bd4c9bcadf5a197fbf9153f77f8e9b
TiagoTNX <tiagotnx@gmail.com>
2022-09-09 02:43:07 -0300

    :recycle: Refatoração do Controller utilizando generics.

--- src/main/java/br/com/tiacademy/vendas/controller/VendedorController.java
+++ src/main/java/br/com/tiacademy/vendas/controller/VendedorController.java
@@ -1,52 +1,11 @@
 package br.com.tiacademy.vendas.controller;
 
+import br.com.tiacademy.vendas.core.crud.CrudController;
 import br.com.tiacademy.vendas.domain.Vendedor;
-import br.com.tiacademy.vendas.service.VendedorService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Objects;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/vendedor")
-public class VendedorController {
-
-    @Autowired
-    private VendedorService vendedorService;
-
-    @GetMapping
-    public ResponseEntity<List<Vendedor>> listar() {
-        var vendedores = vendedorService.listar();
-        return ResponseEntity.ok(vendedores);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Vendedor> especifico(@PathVariable("id") Long id) {
-        var resultado = vendedorService.porID(id);
-
-        if (Objects.isNull(resultado)) {
-            return ResponseEntity.notFound().build();
-        }
-
-        return ResponseEntity.ok(resultado);
-    }
-
-    @PostMapping
-    public ResponseEntity<Vendedor> criar(@RequestBody Vendedor vendedor) {
-        var salvo = vendedorService.criar(vendedor);
-        return ResponseEntity.ok(salvo);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Vendedor> editar(@PathVariable("id") Long id, @RequestBody Vendedor vendedor) {
-        return ResponseEntity.ok(vendedorService.editar(id, vendedor));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> excluir(@PathVariable("id") Long id) {
-        vendedorService.excluir(id);
-        return ResponseEntity.ok().build();
-    }
+public class VendedorController extends CrudController<Vendedor, Long> {
 }
--- src/main/java/br/com/tiacademy/vendas/core/crud/CrudController.java
+++ src/main/java/br/com/tiacademy/vendas/core/crud/CrudController.java
@@ -1,4 +1,48 @@
 package br.com.tiacademy.vendas.core.crud;
 
-public class CrudController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+
+public class CrudController<T, ID> {
+
+    @Autowired
+    protected CrudService<T, ID> service;
+
+    @GetMapping
+    public ResponseEntity<List<T>> listar() {
+        var listaEntidade = service.listar();
+        return ResponseEntity.ok(listaEntidade);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<T> especifico(@PathVariable("id") ID id) {
+        var resultado = service.porID(id);
+
+        if (Objects.isNull(resultado)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(resultado);
+    }
+
+    @PostMapping
+    public ResponseEntity<T> criar(@RequestBody T entidade) {
+        var salvo = service.criar(entidade);
+        return ResponseEntity.ok(salvo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<T> editar(@PathVariable("id") ID id, @RequestBody T entidade) {
+        return ResponseEntity.ok(service.editar(id, entidade));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable("id") ID id) {
+        service.excluir(id);
+        return ResponseEntity.ok().build();
+    }
 }
